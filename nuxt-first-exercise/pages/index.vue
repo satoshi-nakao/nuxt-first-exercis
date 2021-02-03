848a017435b4c317ee18482378c4284f55d04865
Your Name <you@example.com>
2021-02-03 21:40:44 +0900

    Nuxtjsの学習の記録として残します

---
+++ nuxt-first-exercise/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <p>{{ message }}</p>
+    <hr>
+    <h1>Top Page</h1>
+    <router-link to="/mypage">My Page</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message:"Hello World!"
+    }
+  }
+}
+</script>
+
